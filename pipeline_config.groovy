b965c8e81049df892bcbc6c79a97cc9ee841f993
Chang <590114@bah.com>
2019-01-25 08:31:35 -0500

    Update groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,4 @@
-sdp_image_repository = "http://0.0.0.0:5000"
+sdp_image_repository = "http://localhost:5000"
 sdp_image_repository_credential = "sdp-docker-registry"
 application_image_repository = "0.0.0.0:5000"
 application_image_repository_credential = "sdp-docker-registry"
